eeb4684fbcebe8d92618c0b93c2d71221d803193
Malik Shaukat <malik.shaukat@halliburton.com>
2026-08-30 18:43:59 +0500

    Removed student_reg

--- student_reg/configure.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$conn = mysqli_connect('localhost:3306', 'watimcom_develop', 'Watim@321123$AH@', 'watimcom_website');
-// $conn = mysqli_connect('localhost', 'root', '', 'watimcom_website');
- 
-
-
-?>
